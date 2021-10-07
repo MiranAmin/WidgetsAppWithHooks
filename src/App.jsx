2b0516534a157b24a14d43a17fadbd96b3056e63
Miran Amin <miranamin@hotmail.co.uk>
2021-10-07 22:50:05 +0100

    Some initial steps to make the search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Accordion from "./components/Accordion";
-
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 const items = [
   {
     title: "what is react?",
@@ -19,7 +19,8 @@ const items = [
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
